21dab5b201f40b52ccf78de2647013d71d6b05a7
Alex Sanchez <alex.sanchez@alertmedia.com>
2020-05-07 13:48:04 -0500

    Chaning getHandler() to getHandler so that SNS messages can trigger Python lambdas

--- src/sns-adapter.ts
+++ src/sns-adapter.ts
@@ -140,7 +140,7 @@ export class SNSAdapter implements ISNSAdapter {
                     this.sent(response);
                 }
             };
-            const maybePromise = getHandler()(event, this.createLambdaContext(fn, sendIt), sendIt);
+            const maybePromise = getHandler(event, this.createLambdaContext(fn, sendIt), sendIt);
             if (maybePromise && maybePromise.then) {
                 maybePromise
                     .then(response => sendIt(null, response))
